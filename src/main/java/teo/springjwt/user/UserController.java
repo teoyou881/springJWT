2945c24e849efdaf54d031c91f2c5e982e1cddda
Teo_You <teoyou881@gmail.com>
2025-05-24 16:17:53 -0400

    add find

--- src/main/java/teo/springjwt/user/UserController.java
+++ src/main/java/teo/springjwt/user/UserController.java
@@ -3,6 +3,7 @@ package teo.springjwt.user;
 import java.security.Principal;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import teo.springjwt.user.dto.RegisterDTO;
@@ -30,5 +31,13 @@ public class UserController {
   public String user(Principal principal) {
     System.out.println(principal);
     return "admin Controller";
+  }
+
+  // 이렇게 조회할 경우, 트랜잭션 범위 밖에서 user를 찾는다.
+  // 따라서, 영속성 컨텍스트에서 관리되지 않는 상태. detached.
+  // dirty checking 되지 않는다.
+  @GetMapping("/user/{id}")
+  public String findUser(@PathVariable("id") UserEntity user) {
+    return user.getUsername();
   }
 }
