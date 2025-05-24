717548dfa731876057b8e9b1b83d8730f185497e
Teo_You <teoyou881@gmail.com>
2025-05-24 02:23:54 -0400

    first commit

---
+++ src/test/java/teo/springjwt/SpringJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package teo.springjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJwtApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
