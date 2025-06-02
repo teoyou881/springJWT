f8fb788959627b213757642360615669093bb3ef
Teo_You <teoyou881@gmail.com>
2025-06-02 08:07:32 -0400

    fix getOptionValues in ValueController

--- src/main/java/teo/springjwt/product/controller/ValueController.java
+++ src/main/java/teo/springjwt/product/controller/ValueController.java
@@ -1,13 +1,10 @@
 package teo.springjwt.product.controller;
 
-import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import teo.springjwt.product.dto.CreateGroupDTO;
 import teo.springjwt.product.entity.OptionValueEntity;
 import teo.springjwt.product.service.ValueService;
 
@@ -19,7 +16,7 @@ public class ValueController {
   private final ValueService valueService;
 
   @GetMapping
-  public List<OptionValueEntity> getOptionValues(@Valid @RequestBody CreateGroupDTO requestDto) {
+  public List<OptionValueEntity> getOptionValues() {
     return valueService.getValues();
   }
 }
