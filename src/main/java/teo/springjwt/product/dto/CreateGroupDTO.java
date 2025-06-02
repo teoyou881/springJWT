30a49c9f4f27186b846942ca78bb1c89aa3bee29
Teo_You <teoyou881@gmail.com>
2025-06-02 08:06:56 -0400

    add noargsConstructor for binding json

--- src/main/java/teo/springjwt/product/dto/CreateGroupDTO.java
+++ src/main/java/teo/springjwt/product/dto/CreateGroupDTO.java
@@ -4,9 +4,11 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor  // JSON 바인딩을 위한 기본 생성자
 public class CreateGroupDTO {
   @NotBlank(message = "옵션 이름은 필수입니다")
   private String name;
--- src/main/java/teo/springjwt/product/dto/CreateValueDTO.java
+++ src/main/java/teo/springjwt/product/dto/CreateValueDTO.java
@@ -1,8 +1,20 @@
 package teo.springjwt.product.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import teo.springjwt.product.entity.OptionGroupEntity;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor  // JSON 바인딩을 위한 기본 생성자
 public class CreateValueDTO {
+  @NotBlank(message = "옵션 값은 필수입니다")
+  private String valueName;
+
+  @NotNull(message = "옵션 그룹 정보는 필수입니다")
+  private OptionGroupEntity optionGroup; // 연관관계의 주인
 
 }
